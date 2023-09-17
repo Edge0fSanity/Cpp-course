c1f8ef884b7da3dcae4a8e18e44f3222538db9ef
Nikita Povolotsky <99560164+Edge0fSanity@users.noreply.github.com>
2023-09-17 04:54:27 +0300

    Update Lab-1.cpp

--- Lab-1.cpp
+++ Lab-1.cpp
@@ -8,17 +8,28 @@ int main()
     cin >> n;
     switch(n) {
         case 1:
-            cout << "Введите 2 числа" << endl;
-            float a;
-            float b;
-            cin >> a >> b;
-            cout << "Сумма " << a+b << endl;
-            cout << "Разность " << a-b << endl;
-            cout << "Произведение " << a*b << endl;
-            cout << "Частное " << a/b << endl;
-            cout << "Ср. арифметическое " << (a+b)/2 << endl;
+        cout << "Введите 2 числа" << endl;
+        float a;
+        float b;
+        cin >> a >> b;
+        cout << "Сумма " << a+b << endl;
+        cout << "Разность " << a-b << endl;
+        cout << "Произведение " << a*b << endl;
+        cout << "Частное " << a/b << endl;
+        cout << "Ср. арифметическое " << (a+b)/2 << endl;
         break;
         case 2:
+        int bread = 0;
+        int colbasa = 0;
+        int cheese = 0;
+        int kefir = 0;
+        float price_bread = 10.10;
+        float price_colbasa = 20.20;
+        float price_cheese = 30.30;
+        float price_kefir = 40.40;
+        cout << "";
+        cout << 
+        
         
     }
     return 0;
